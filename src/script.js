58698aabdbf13fe68c520639e13591e09c08e3c5
holus-bolus <ustinovoleksij@gmail.com>
2022-11-01 17:44:14 +0200

    Added a remove method

--- src/script.js
+++ src/script.js
@@ -3,27 +3,50 @@ const list = document.querySelector(".list");
 const button = document.querySelector(".form__button");
 let firstName = document.querySelector('.first-name');
 let lastName = document.querySelector('.last-name');
-const removeElement = "╳";
-const item = document.querySelectorAll('li');
+const deleteButton = document.querySelector(".delete__element")
+
+let arrayOfUsers = [];
+if (localStorage.getItem("list")) {
+  arrayOfUsers = JSON.parse(localStorage.getItem("list"));
+  displayUser();
+}
+button.addEventListener("click", () => {
+  let user = {
+    name: firstName.value,
+    lastName: lastName.value
+  }
+  arrayOfUsers.push(user);
+  displayUser();
+  localStorage.setItem("list", JSON.stringify(arrayOfUsers));
+});
 
 
-button.addEventListener("click", (event) => {
-  event.preventDefault();
-  const listItem = document.createElement("li");
-  listItem.append(firstName.value, " ", lastName.value, "  ", removeElement);
-  list.append(listItem);
-  firstName.value = " ";
-  lastName.value = " ";
-})
+function displayUser() {
+  let displayUser = "";
+  arrayOfUsers.forEach((item, index) => {
+    displayUser += `<li id="item_${index}">
+                    <p>${item.name}</p>
+                    <p>${item.lastName}</p>
+                    <button class="delete__element">Delete </button>
+                    </li>`;
+    list.innerHTML = displayUser;
+    firstName.value = "";
+    lastName.value = "";
+  })
+}
 
-list.onclick = (event) => {
-  let target = event.target;
-  if (target === removeElement) {
-    item.forEach((element) => {
-      element.pop();
-    })
+function deleteUser(event) {
+  if (event.target.className === "delete__element") {
+    const parentElement = event.target.closest('li');
+    parentElement.remove();
   }
 }
+
+list.addEventListener('click', deleteUser);
+
+
+
+
 
 
 
