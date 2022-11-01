f0de0755ff03ca0c6fe722548880705974ca9124
holus-bolus <ustinovoleksij@gmail.com>
2022-11-01 11:47:10 +0200

    Added an element adding ion

--- src/script.js
+++ src/script.js
@@ -1,1 +1,17 @@
-console.log(1);
+const form = document.querySelector('.form');
+const list = document.querySelector(".list");
+const button = document.querySelector(".form__button");
+let firstName = document.querySelector('.first-name');
+let lastName = document.querySelector('.last-name');
+const removeElement = "╳";
+
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  const listItem = document.createElement("li");
+  listItem.append(firstName.value, " ", lastName.value, "  ", removeElement);
+  list.append(listItem);
+  firstName.value = " ";
+  lastName.value = " ";
+});
+
